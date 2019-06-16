11415ea92eca105b79a818b9d598950a667a78d4
Daniel Sirz <daniel.sirz@gmail.com>
2019-06-16 08:19:16 +0200

    chore: use dataservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'never-bind-a-function-in-a-template';
+export class AppComponent implements OnInit {
+  title = 'Never bind a function in a template Demo';
+  data: Array<object>;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.get().subscribe(res => {
+      this.data = res;
+    });
+  }
+
 }
